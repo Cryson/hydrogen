b8dd25952189455fab661d7d37cc3cd8e362e368
Cryson <billykwooten@gmail.com>
2017-07-16 10:13:07 -0400

    Removing test comments

--- hydrogen/hydrogen.py
+++ hydrogen/hydrogen.py
@@ -73,8 +73,8 @@ def hydrogen():
 	todayhours = int(datetime.datetime.now().strftime("%H"))  # Hour of the day in 24 hour format
 	today = str(datetime.date.today())
 
-	#if todayhours not in range(14, 19):  # Check if it is peak hours for CobbEMC
-	#	return 2
+	if todayhours not in range(14, 19):  # Check if it is peak hours for CobbEMC
+		return 2
 
 	if minutes < 288:
 		logging.info("Messages cache file is not 288 minutes old, reading from cache file")
